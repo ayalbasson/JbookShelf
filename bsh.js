1311447d6cd7e5c8402a9a0c075f483e4b75269c
asafz83 <asafz83@gmail.com>
2014-11-27 20:19:50 +0200

    Update bsh.js

--- bsh.js
+++ bsh.js
@@ -7,7 +7,16 @@ $(document).ready(function(){
     newBook.bookName = $(".addBookForm input:nth(0)").val();
     newBook.authorName = $(".addBookForm input:nth(1)").val();
     newBook.score = $(".addBookForm input:nth(2)").val();
-    console.log(newBook);
+    //console.log(newBook);
+    var newLi = $("<li>");
+    var nameSpan = $("<span>");
+    nameSpan.html(newBook.bookName);
+    var authorSpan = $("<span>");
+    authorSpan.html(newBook.authorName);
+    var scoreSpan = $("<span>");
+    scoreSpan.html(newBook.score);
+    newLi.append(nameSpan).append(authorSpan).append(scoreSpan);
+    $("ul").append(newLi);
     books.push(newBook);
   });
 });
