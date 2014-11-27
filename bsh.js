3f2a35313d3ec44d6e09db4481379861dcefd172
asafz83 <asafz83@gmail.com>
2014-11-27 21:29:55 +0200

    Update bsh.js

--- bsh.js
+++ bsh.js
@@ -7,14 +7,21 @@ $(document).ready(function(){
     newBook.bookName = $(".addBookForm input:nth(0)").val();
     newBook.authorName = $(".addBookForm input:nth(1)").val();
     newBook.score = $(".addBookForm input:nth(2)").val();
+    newBook.id = "book_" + new Date().getTime();
     //console.log(newBook);
     var newLi = $("<li>");
+    newLi.attr("id", newBook.id);
     var nameSpan = $("<div>");
     nameSpan.html(newBook.bookName);
     var authorSpan = $("<div>");
     authorSpan.html(newBook.authorName);
     var scoreSpan = $("<div>");
     scoreSpan.html(newBook.score);
+    var xSpan = $("<div>");
+    xSpan.html("X");
+    xSpan.on("click", function(){
+      $("#" + newBook.id).remove();
+    });
     newLi.append(nameSpan).append(authorSpan).append(scoreSpan);
     $("ul").append(newLi);
     books.push(newBook);
