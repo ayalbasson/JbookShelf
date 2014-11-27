43f650886f47b493efeaeddbe65c772594dc7f48
asafz83 <asafz83@gmail.com>
2014-11-27 19:17:21 +0200

    Update bsh.js

--- bsh.js
+++ bsh.js
@@ -1,1 +1,10 @@
 console.log("Welcome");
+$(document).ready(function(){
+  $("addBookForm button").on("click", function(){
+    var newBook = {};
+    newBook.bookName = $("addBookForm input:nth(0)").val();
+    newBook.authorName = $("addBookForm input:nth(1)").val();
+    newBook.score = $("addBookForm input:nth(2)").val();
+    console.log(newBook);
+  });
+});
